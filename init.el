d8e616f2de76700c2fbdc47d66ee397ab50ea1de
air <airmanghigh@gmail.com>
2023-03-01 20:10:06 +0800

    layer: add several layers

--- init.el
+++ init.el
@@ -38,22 +38,31 @@ This function should only modify configuration layer settings."
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
-     ;; better-defaults
+     auto-completion
+     better-defaults
      emacs-lisp
-     ;; git
+     git
      helm
-     ;; lsp
-     ;; markdown
+     lsp
+     markdown
      multiple-cursors
-     ;; org
+     org
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
+     spell-checking
+     syntax-checking
      ;; version-control
-     treemacs)
+     treemacs
+     ;; programming languages
+     c-c++
+     emacs-lisp
+     latex
+     python
+     '(ranger :variables
+              ranger-show-preview t)
+     shell-scripts
+     yaml)
 
 
    ;; List of additional packages that will be installed without being wrapped
